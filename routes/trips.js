32d60d8b629c227488c266a4a2a540cff83d8625
pierreC <pierrecgdev@gmail.com>
2025-04-15 23:15:38 +0200

    correction format de date

--- routes/trips.js
+++ routes/trips.js
@@ -5,6 +5,8 @@ const { checkBody } = require('../modules/checkBody');
 
 const Trip = require('../models/trips');
 
+
+// route de test 
 router.get('/:cityName', function(req, res) {
     Trip.find({departure: { $regex: new RegExp(req.params.cityName, "i") }})
     .then(data => {
@@ -16,17 +18,20 @@ router.get('/:cityName', function(req, res) {
         })
     });
 
+//route utile
 router.get('/', function(req, res) {
+    const {departure, arrival, date} = req.body
     if(checkBody(req.body,['departure','arrival','date'])){
-        const departure = req.body.departure.trim();
-        const arrival = req.body.arrival.trim();
-        const statLocal = moment(req.body.date).utc()
-        const enLocal = moment(req.body.date).utc()
-        const startDate = statLocal.startOf('day');
-        const endDate = enLocal.endOf('day');
+        const departureTrim = departure.trim();
+        const arrivalTrim = arrival.trim();
+        // const startLocal = moment(date).utc().format()
+        // const endLocal = moment(date).utc().format()
+        //converstion de la date format YYYY-MM-DD en complet et UTC
+        const startDate = moment.utc(date).startOf('day');
+        const endDate = moment.utc(date).endOf('day');
         Trip.find({ 
-            departure: { $regex: new RegExp(`^${departure}$`, "i") },
-            arrival: { $regex: new RegExp(`^${arrival}$`, "i") },
+            departure: { $regex: new RegExp(`^${departureTrim}$`, "i") },
+            arrival: { $regex: new RegExp(`^${arrivalTrim}$`, "i") },
             date: {
                 $gte: startDate,
                 $lte: endDate,
